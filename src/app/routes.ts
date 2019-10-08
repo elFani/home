6f4a142f51210dac51b38b1b91f02bf0b5e5dc59
Phillip Faniola <pfaniola@ucsd.edu>
2019-10-08 08:08:57 -0700

    added 404

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,5 +6,6 @@ import { LayoutTwoComponent } from './layout-two/layout-two.component';
 export const appRoutes: Routes = [
     { path: 'layout-one', component: LayoutOneComponent },
     { path: 'layout-two', component: LayoutTwoComponent },
-    { path: '',   redirectTo: '/layout-one', pathMatch: 'full' }
+    { path: '', redirectTo: '/layout-one', pathMatch: 'full' }
+    // { path: '*', redirectTo: '/layout-one' }
 ];
