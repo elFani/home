364f1689c559a7b109ef2beaf71ee0de02ef3dc7
Phillip Faniola <pfaniola@ucsd.edu>
2019-10-10 09:24:40 -0700

    app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  apptitle = 'Welcome to el folio';
+  apptitle = 'Welcome to netlify';
   username = 'Visitor';
 
   ngOnInit() {
