9a87af879dad2e0dd6c824182ca783b6ab62ad1d
Phillip Faniola <pfaniola@ucsd.edu>
2019-10-15 13:03:24 -0700

    hooked up secondary router

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,7 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { LayoutTwoComponent } from './layout-two/layout-two.component';
 
 export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, children: [
+    { path: 'proj', component: LayoutTwoComponent },
+    { path: 'proj2', component: LayoutTwoComponent}
+  ] },
   { path: 'layout-two', component: LayoutTwoComponent },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
